35e1a08b7dc233a1e7dfb618d70e3ff7278eb92a
Pedro <pedroluislanzabejarano@gmail.com>
2026-02-11 20:10:06 -0400

    cuarto cambio hecho por pedro

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
-"console.log('Hola mundo, este cambio lo hizo pedro');" 
-"console.log('Hola mundo, este es el segundo cambio hecho por pedro');"
-"console.log('Hola mundo, este es el tercer cambio hecho por pedro');"
-"console.log('Hola mundo, este es el cuarto cambio hecho por pedro');"
+"console.log('Hola mundo, este es el quinto cambio hecho por pedro');"
